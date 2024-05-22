ccfbef3beaab75b094f818d5a93a861658c94094
JohnyBoy <jeanvaneck031011@gmail.com>
2024-05-22 13:42:45 +0200

    Testing React Router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
+import { Routes, Route, Link } from 'react-router-dom';
+import Home from "./pages/Home";
+import Projects from './pages/Projects';
+import About from "./pages/About";
 import './App.css';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav className="navbar">
+        <div className="nav-container">
+          <div className="logo">NavBar</div>
+          <ul className="nav-links">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/About">Features</Link>
+            </li>
+            <li>
+              <Link to="/Projects">Services</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      <Routes>
+        <Route path='*' element={<NotFound />}></Route>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/About' element={<About />}></Route>
+        <Route path='/Projects' element={<Projects />}></Route>
+      </Routes>
+    </>
   );
 }
 
